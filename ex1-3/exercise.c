2e222a1ec2dcbded67d85beee462827a49b7b899
Nikita Chebykin <nikita@chebyk.in>
2015-07-30 11:04:41 +0000

    Add ex1-3

---
+++ ex1-3/exercise.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+  float fahr, celsius;
+  float lower, upper, step;
+
+  lower = 0;
+  upper = 300;
+  step = 20;
+
+  fahr = lower;
+
+  printf("The temperature conversion program.\n");
+  printf("FAHR CELSIUS\n");
+
+  while (fahr <= upper) {
+    celsius = (5.0 / 9.0) * (fahr - 32.0);
+    printf("%3.0f %7.2f\n", fahr, celsius);
+    fahr = fahr + step;
+  }
+  return 0;
+}
+
